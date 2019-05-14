3de64e095080411e157764477397dcabc3dfe096
Fadillah98 <17010176@myrp.edu.sg>
2019-05-14 14:51:26 +0800

    Finished

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -10,10 +10,13 @@ import android.util.Log;
 import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
-
 	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
+	private static final String DATABASE_NAME = "revision.db";
+	private static final int DATABASE_VERSION = 1;
+	private static final String TABLE_NOTE = "revision";
+	private static final String COLUMN_ID = "_id";
+	private static final String COLUMN_NOTE_CONTENT = "noteContent";
+	private static final String COLUMN_STARS = "stars";
 
 
 	public DBHelper(Context context) {
@@ -23,7 +26,11 @@ public class DBHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		//TODO CREATE TABLE Note
-
+		String createTableSql = "CREATE TABLE " + TABLE_NOTE +  "("
+				+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ COLUMN_NOTE_CONTENT + " TEXT,"
+				+ COLUMN_STARS + " INTEGER )";
+		db.execSQL(createTableSql);
 	}
 
 	@Override
@@ -34,37 +41,64 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	public void insertNote(String noteContent, int stars) {
 		//TODO insert the data into the database
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_NOTE_CONTENT, noteContent);
+		values.put(COLUMN_STARS, stars);
+		db.insert(TABLE_NOTE, null, values);
+		db.close();
 	}
 
 	public ArrayList<Note> getAllNotes() {
 		//TODO return records in Java objects
+		ArrayList<Note> notes = new ArrayList<Note>();
+		String selectQuery = "SELECT " + COLUMN_ID + ", "
+				+ COLUMN_NOTE_CONTENT + ", "
+				+ COLUMN_STARS
+				+ " FROM " + TABLE_NOTE;
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		if (cursor.moveToFirst()) {
+			do {
+				int id = cursor.getInt(0);
+				String noteContent = cursor.getString(1);
+				int stars = cursor.getInt(2);
+				Note obj = new Note(id, noteContent, stars);
+				notes.add(obj);
+			} while (cursor.moveToNext());
+		}
+		cursor.close();
+		db.close();
+		return notes;
 	}
 
-    public ArrayList<String> getNoteContent() {
-        //TODO return records in Strings
+	public ArrayList<String> getNoteContent() {
+		//TODO return records in Strings
 
 		// Create an ArrayList that holds String objects
-        ArrayList<String> notes = new ArrayList<String>();
-        // Select all the notes' content
-        String selectQuery = "";
+		ArrayList<String> notes = new ArrayList<String>();
+		// Select all the notes' content
+		String selectQuery = "";
 
-        // Get the instance of database to read
-        SQLiteDatabase db = this.getReadableDatabase();
-        // Run the SQL query and get back the Cursor object
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        // moveToFirst() moves to first row
-        if (cursor.moveToFirst()) {
-            // Loop while moveToNext() points to next row and returns true;
-            // moveToNext() returns false when no more next row to move to
-            do {
+		// Get the instance of database to read
+		SQLiteDatabase db = this.getReadableDatabase();
+		// Run the SQL query and get back the Cursor object
+		Cursor cursor = db.rawQuery(selectQuery, null);
+		// moveToFirst() moves to first row
+		if (cursor.moveToFirst()) {
+			// Loop while moveToNext() points to next row and returns true;
+			// moveToNext() returns false when no more next row to move to
+			do {
 
+				notes.add(cursor.getString(0));
+			} while (cursor.moveToNext());
+		}
+		// Close connection
+		cursor.close();
+		db.close();
 
-            } while (cursor.moveToNext());
-        }
-        // Close connection
-        cursor.close();
-        db.close();
-
-        return notes;
-    }
+		return notes;
+	}
 }
